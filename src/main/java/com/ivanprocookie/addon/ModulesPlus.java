61a1037ebe0b4c7a3bd376a4f5104476467e63d9
OrangeMan-432 <167663529+OrangeMan-432@users.noreply.github.com>
2024-07-24 21:30:12 +0000

    make 6 new modules named OtherKey*

--- src/main/java/com/ivanprocookie/addon/ModulesPlus.java
+++ src/main/java/com/ivanprocookie/addon/ModulesPlus.java
@@ -2,16 +2,16 @@ package com.ivanprocookie.addon;
 
 // import com.ivanprocookie.addon.commands.CommandExample;
 // import com.ivanprocookie.addon.hud.HudExample;
-import com.ivanprocookie.addon.modules.wasd.WKey;
-import com.ivanprocookie.addon.modules.wasd.AKey;
-import com.ivanprocookie.addon.modules.wasd.SKey;
-import com.ivanprocookie.addon.modules.wasd.DKey;
 import com.ivanprocookie.addon.modules.OtherKey1;
 import com.ivanprocookie.addon.modules.OtherKey2;
 import com.ivanprocookie.addon.modules.OtherKey3;
 import com.ivanprocookie.addon.modules.OtherKey4;
 import com.ivanprocookie.addon.modules.OtherKey5;
 import com.ivanprocookie.addon.modules.OtherKey6;
+import com.ivanprocookie.addon.modules.wasd.WKey;
+import com.ivanprocookie.addon.modules.wasd.AKey;
+import com.ivanprocookie.addon.modules.wasd.SKey;
+import com.ivanprocookie.addon.modules.wasd.DKey;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
@@ -36,6 +36,12 @@ public class ModulesPlus extends MeteorAddon {
 		Modules.get().add(new AKey());
 		Modules.get().add(new SKey());
 		Modules.get().add(new DKey());
+        Modules.get().add(new OtherKey1());
+        Modules.get().add(new OtherKey2());
+        Modules.get().add(new OtherKey3());
+        Modules.get().add(new OtherKey4());
+        Modules.get().add(new OtherKey5());
+        Modules.get().add(new OtherKey6());
 
         // Commands
         // Commands.add(new CommandExample());

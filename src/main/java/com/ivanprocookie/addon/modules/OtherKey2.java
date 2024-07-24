211c48ef3f3d1abba7b2802f64a2b2a04bb8a8fe
OrangeMan-432 <167663529+OrangeMan-432@users.noreply.github.com>
2024-07-24 20:48:58 +0000

    add 6 other keys

---
+++ src/main/java/com/ivanprocookie/addon/modules/OtherKey2.java
@@ -0,0 +1,21 @@
+package com.ivanprocookie.addon.modules;
+
+import com.ivanprocookie.addon.ModulesPlus;
+import meteordevelopment.meteorclient.events.render.Render3DEvent;
+import meteordevelopment.meteorclient.renderer.ShapeMode;
+import meteordevelopment.meteorclient.settings.ColorSetting;
+import meteordevelopment.meteorclient.settings.DoubleSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+
+public class OtherKey2 extends Module {
+
+   public OtherKey2() {
+      super(ModulesPlus.CATEGORY, "OtherKey2", "A Standalone Module Purely for use with Starscript");
+   }
+
+}
---
+++ src/main/java/com/ivanprocookie/addon/modules/OtherKey5.java
@@ -0,0 +1,21 @@
+package com.ivanprocookie.addon.modules;
+
+import com.ivanprocookie.addon.ModulesPlus;
+import meteordevelopment.meteorclient.events.render.Render3DEvent;
+import meteordevelopment.meteorclient.renderer.ShapeMode;
+import meteordevelopment.meteorclient.settings.ColorSetting;
+import meteordevelopment.meteorclient.settings.DoubleSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+
+public class OtherKey5 extends Module {
+
+   public OtherKey5() {
+      super(ModulesPlus.CATEGORY, "OtherKey5", "A Standalone Module Purely for use with Starscript");
+   }
+
+}
